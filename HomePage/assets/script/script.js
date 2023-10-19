2b860e7c9632d735765472a13af3920e08dd229e
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-19 19:04:37 -0300

    Validações com javascript

--- HomePage/assets/script/script.js
+++ HomePage/assets/script/script.js
@@ -1,11 +1,63 @@
 let nome = document.getElementById('nome')
-let email = document.getElementById('email')
+let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
+let mapa = document.querySelector('#mapa')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
 
-function verificarCampos(){
-    if(nome.value == "" || email.value == "" || assunto.value == ""){
-        alert("Campo obrigatório!")
+
+function validaNome(){
+    let txtNome = document.querySelector('#txtNome')
+    if(nome.value.length < 3){
+        txtNome.innerHTML = 'Nome inválido!'
+        txtNome.style.color = 'red'
+        txtNome.style.display = 'block'
     }else{
-        alert("Enviado com sucesso!")
+        txtNome.style.display = 'none'
+        nomeOk = true
     }
+}
+
+function validaEmail(){
+    let txtEmail = document.querySelector('#txtEmail')
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){
+        txtEmail.innerHTML = 'E-mail inválido!'
+        txtEmail.style.color = 'red'
+        txtEmail.style.display = 'block'
+    }else{
+        txtEmail.style.display = 'none'
+        emailOk = true
+    }
+}
+
+function validaAssunto(){
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if(assunto.value.length >= 100){
+        txtAssunto.innerHTML = 'Digite no máximo 100 caracteres!'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    }else{
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+
+function enviar(){
+    if(nomeOk == true && emailOk == true && assuntoOk == true){
+        alert("Formulário enviado com sucesso!")
+    }else{
+        alert("Preencha o formulário corretamente!")
+    }
+}
+
+function mapaZoom(){
+    mapa.style.width = '800px'
+    mapa.style.height = '600px'
+}
+
+function mapaNormal(){
+    mapa.style.width = '400px'
+    mapa.style.height = '250px'
 }
